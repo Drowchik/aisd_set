2165269c967601e893d13b7f8fd2da53e7f5a79d
natal <natal@CLOWN>
2024-02-20 11:40:28 +0400

    add_all_task

--- console/console.cpp
+++ console/console.cpp
@@ -174,6 +174,7 @@ int main() {
 
 	//TASK 3
 
+	//insert
 	//double time_count = 0;
 	//for (size_t i = 0; i < 1000; i++) {
 	//	Set<int> a;
@@ -190,5 +191,189 @@ int main() {
 	//cout << "Average time, 1000 nums: "<<time_count / 1000 << " seconds"<< endl;
 	//Average time, 1000 nums: 6.8722e-06 seconds
 
+	//erase
+	//double time_count = 0;
+	//for (size_t i = 0; i < 1000; i++) {
+	//	Set<int> a;
+	//	vector<int> vec = random(-10000, 10000, 1000, i);
+	//	size_t size = vec.size();
+	//	for (size_t j = 0; j < size; j++) {
+	//		a.insert(vec[j]);
+	//	}
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	a.erase(random(-1000, 1000, i));
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: " << time_count / 1000 << " seconds" << endl;
+	//Average time, 1000 nums: 6.8336e-06 seconds
+
+	//vector insert
+	//double time_count = 0;
+	//for (size_t i = 0; i < 1000; i++) {
+	//	vector<int> vec = random(-10000, 10000, 1000, i);
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	vec.push_back(random(-1000, 1000, i));
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: "<<time_count / 1000 << " seconds"<< endl;
+	//Average time, 1000 nums: 8.6143e-06 seconds
+
+	
+	//vector erase()
+	//double time_count = 0;
+	//for (size_t i = 0; i < 1000; i++) {
+	//	vector<int> vec = random(-10000, 10000, 1000, i);
+	//	size_t size = vec.size();
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	int val = random(-1000, 1000, i);
+	//	for (size_t j = 0; j < size; j++) {
+	//		if (vec[j] == val) {
+	//			vec.erase(vec.begin() + j);
+	//			break;
+	//		}
+	//	}
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: " << time_count / 1000 << " seconds" << endl;
+	//Average time, 1000 nums: 1.28001e-05 seconds
+
+	//10.000
+
+	//insert
+	//double time_count = 0;
+	//for (size_t i = 0; i < 1000; i++) {
+	//	Set<int> a;
+	//	vector<int> vec = random(-100000, 100000, 10000, i);
+	//	size_t size = vec.size();
+	//	for (size_t j = 0; j < size; j++) {
+	//		a.insert(vec[j]);
+	//	}
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	a.insert(random(-1000, 1000, i));
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: "<<time_count / 1000 << " seconds"<< endl;
+	//Average time, 1000 nums: 1.00025e-05 seconds
+
+	//erase
+	//double time_count = 0;
+	//for (size_t i = 0; i < 1000; i++) {
+	//	Set<int> a;
+	//	vector<int> vec = random(-100000, 100000, 10000, i);
+	//	size_t size = vec.size();
+	//	for (size_t j = 0; j < size; j++) {
+	//		a.insert(vec[j]);
+	//	}
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	a.erase(random(-10000, 10000, i));
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: " << time_count / 1000 << " seconds" << endl;
+	//Average time, 1000 nums: 8.3108e-06 seconds
+
+	//vector insert
+	//double time_count = 0;
+	//for (size_t i = 0; i < 1000; i++) {
+	//	vector<int> vec = random(-100000, 100000, 10000, i);
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	vec.push_back(random(-10000, 10000, i));
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: "<<time_count / 1000 << " seconds"<< endl;
+	//Average time, 1000 nums: 1.17133e-05 seconds
+
+
+	//vector erase()
+	//double time_count = 0;
+	//for (size_t i = 0; i < 1000; i++) {
+	//	vector<int> vec = random(-100000, 100000, 10000, i);
+	//	size_t size = vec.size();
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	int val = random(-10000, 10000, i);
+	//	for (size_t j = 0; j < size; j++) {
+	//		if (vec[j] == val) {
+	//			vec.erase(vec.begin() + j);
+	//			break;
+	//		}
+	//	}
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: " << time_count / 1000 << " seconds" << endl;
+	//Average time, 1000 nums: 5.85246e-05 seconds
+
+	//100.000
+	// 
+	//insert
+	//double time_count = 0;
+	//for (size_t i = 0; i < 1000; i++) {
+	//	Set<int> a;
+	//	vector<int> vec = random(-1000000, 1000000, 100000, i);
+	//	size_t size = vec.size();
+	//	for (size_t j = 0; j < size; j++) {
+	//		a.insert(vec[j]);
+	//	}
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	a.insert(random(-10000, 10000, i));
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: "<<time_count / 1000 << " seconds"<< endl;
+	//Average time, 1000 nums: 9.7221e-06 seconds
+
+	//erase
+	//double time_count = 0;
+	//for (size_t i = 0; i < 1000; i++) {
+	//	Set<int> a;
+	//	vector<int> vec = random(-1000000, 1000000, 100000, i);
+	//	size_t size = vec.size();
+	//	for (size_t j = 0; j < size; j++) {
+	//		a.insert(vec[j]);
+	//	}
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	a.erase(random(-100000, 100000, i));
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: " << time_count / 1000 << " seconds" << endl;
+	//Average time, 1000 nums: 9.9146e-06 seconds
+
+	//vector insert
+	//double time_count = 0;
+	//for (size_t i = 0; i < 1000; i++) {
+	//	vector<int> vec = random(-1000000, 1000000, 100000, i);
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	vec.push_back(random(-100000, 100000, i));
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: "<<time_count / 1000 << " seconds"<< endl;
+	//Average time, 1000 nums: 7.5586e-06 seconds
+
+
+	//vector erase()
+	//double time_count = 0;
+	//for (size_t i = 0; i < 1000; i++) {
+	//	vector<int> vec = random(-1000000, 1000000, 100000, i);
+	//	size_t size = vec.size();
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	int val = random(-100000, 100000, i);
+	//	for (size_t j = 0; j < size; j++) {
+	//		if (vec[j] == val) {
+	//			vec.erase(vec.begin() + j);
+	//			break;
+	//		}
+	//	}
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: " << time_count / 1000 << " seconds" << endl;
+	//Average time, 1000 nums: 0.00033017 seconds
 	return 0;
 }
