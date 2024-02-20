56527de2f3494ba27cdf7232d1b51ef03fe594d4
natal <natal@CLOWN>
2024-02-20 11:09:57 +0400

    add_task1_and_task2

--- console/console.cpp
+++ console/console.cpp
@@ -1,12 +1,194 @@
 #include <iostream>
 #include <aisd_set/aisd_set.h>
-
+#include <chrono>
 using namespace std;
 using namespace set_realization;
 
 int main() {
-	
 
+	//TASK 1
+	//double time_count = 0;
+	//for (size_t i = 0; i < 100; i++) {
+	//	Set<int> a;
+	//	vector<int> vec = random(-10000, 10000, 1000, i);
+	//	size_t size = vec.size();
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	for (size_t j = 0; j < size; j++) {
+	//		a.insert(vec[j]);
+	//	}
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: "<<time_count / 100 << " seconds"<< endl;
+	//Average time, 1000 nums: 0.000195106 seconds														
+
+	//double time_count = 0;
+	//for (size_t i = 0; i < 100; i++) {
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	vector<int> vec = random(-10000, 10000, 1000, i);
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time vector, 1000 nums: " << time_count / 100 << " seconds" << endl;
+	//Average time vector, 1000 nums: 0.00011154 seconds
+	// 
+	//double time_count = 0;
+	//for (size_t i = 0; i < 100; i++) {
+	//	Set<int> a;
+	//	vector<int> vec = random(-100000, 100000, 10000, i);
+	//	size_t size = vec.size();
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	for (size_t j = 0; j < size; j++) {
+	//		a.insert(vec[j]);
+	//	}
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average tima, 10000 nums: " << time_count / 100 << " seconds" << endl;
+	//Average tima, 10000 nums: 0.00262845 seconds
+
+	//double res = 0;
+	//double time_count = 0;
+	//for (size_t i = 0; i < 100; i++) {
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	vector<int> vec = random(-100000, 100000, 10000, i);
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time vector, 10000 nums: " << time_count / 100 << " seconds" << endl;
+	//Average time vector, 10000 nums: 0.00102622 seconds
+	// 
+
+	//double time_count = 0;
+	//for (size_t i = 0; i < 100; i++) {
+	//	Set<int> a;
+	//	vector<int> vec = random(-1000000, 1000000, 100000, i);
+	//	size_t size = vec.size();
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	for (size_t j = 0; j < size; j++) {
+	//		a.insert(vec[j]);
+	//	}
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average tima, 100000 nums: " << time_count / 100 << " seconds" << endl;
+	//Average tima, 100000 nums: 0.0442981 seconds
+	// 
+	//double time_count = 0;
+	//for (size_t i = 0; i < 100; i++) {
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	vector<int> vec = random(-1000000, 1000000, 100000, i);
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time vector, 10000 nums: " << time_count / 100 << " seconds" << endl;
+	//answer 0.192364
+
+	//2 TASK
+	
+	//Set<int> a;
+	//double res = 0;
+	//vector<int> vec = random(-10000, 10000, 1000, 0);
+	//size_t size = vec.size();
+	//for (size_t j = 0; j < size; j++) {
+	//	a.insert(vec[j]);
+	//}
+	//auto start_time = std::chrono::high_resolution_clock::now();
+	//for (size_t j = 0; j < size; j++) {
+	//	a.contains(random(-10000, 10000, j));
+	//}
+	//auto end = std::chrono::high_resolution_clock::now();
+	//res = std::chrono::duration<double>(end - start_time).count();
+	//cout << "Average contains time set, 1000 nums: " << res / 1000 << " seconds" << endl;
+	//Average contains time set, 1000 nums: 6.3815e-06 seconds
+	// 
+	//VECTOR
+	//double res = 0;
+	//vector<int> vec = random(-10000, 10000, 1000, 0);
+	//size_t size = vec.size();
+	//auto start_time = std::chrono::high_resolution_clock::now();
+	//for (size_t j = 0; j < size; j++) {
+	//	int cnt = count(vec.begin(), vec.end(), random(-10000, 10000, j));
+	//}
+	//auto end = std::chrono::high_resolution_clock::now();
+	//res = std::chrono::duration<double>(end - start_time).count();
+	//cout << "Average contains time vector, 1000 nums: " << res / 1000 << " seconds" << endl;
+	//Average contains time vector, 1000 nums: 1.2415e-05 seconds
+
+	//Set<int> a;
+	//double res = 0;
+	//vector<int> vec = random(-100000, 100000, 10000, 0);
+	//size_t size = vec.size();
+	//for (size_t j = 0; j < size; j++) {
+	//	a.insert(vec[j]);
+	//}
+	//auto start_time = std::chrono::high_resolution_clock::now();
+	//for (size_t j = 0; j < 1000; j++) {
+	//	a.contains(random(-100000, 100000, j));
+	//}
+	//auto end = std::chrono::high_resolution_clock::now();
+	//res = std::chrono::duration<double>(end - start_time).count();
+	//cout << "Average contains time set, 1000 nums: " << res / 1000 << " seconds" << endl;
+	//Average contains time set, 1000 nums: 5.9438e-06 seconds
+
+	//double res = 0;
+	//vector<int> vec = random(-100000, 100000, 10000, 0);
+	//size_t size = vec.size();
+	//auto start_time = std::chrono::high_resolution_clock::now();
+	//for (size_t j = 0; j < 1000; j++) {
+	//	int cnt = count(vec.begin(), vec.end(), random(-100000, 100000, j));
+	//}
+	//auto end = std::chrono::high_resolution_clock::now();
+	//res = std::chrono::duration<double>(end - start_time).count();
+	//cout << "Average contains time vector, 1000 nums: " << res / 1000 << " seconds" << endl;
+	//Average contains time vector, 1000 nums: 1.39657e-05 seconds
+	
+	//Set<int> a;
+	//double res = 0;
+	//vector<int> vec = random(-1000000, 1000000, 100000, 0);
+	//size_t size = vec.size();
+	//for (size_t j = 0; j < size; j++) {
+	//	a.insert(vec[j]);
+	//}
+	//auto start_time = std::chrono::high_resolution_clock::now();
+	//for (size_t j = 0; j < 1000; j++) {
+	//	a.contains(random(-1000000, 1000000, j));
+	//}
+	//auto end = std::chrono::high_resolution_clock::now();
+	//res = std::chrono::duration<double>(end - start_time).count();
+	//cout << "Average contains time set, 1000 nums: " << res / 1000 << " seconds" << endl;
+	//Average contains time set, 1000 nums: 7.0102e-06 seconds
+	// 
+	//double res = 0;
+	//vector<int> vec = random(-1000000, 1000000, 100000, 0);
+	//size_t size = vec.size();
+	//auto start_time = std::chrono::high_resolution_clock::now();
+	//for (size_t j = 0; j < 1000; j++) {
+	//	int cnt = count(vec.begin(), vec.end(), random(-1000000, 1000000, j));
+	//}
+	//auto end = std::chrono::high_resolution_clock::now();
+	//res = std::chrono::duration<double>(end - start_time).count();
+	//cout << "Average contains time vector, 1000 nums: " << res / 1000 << " seconds" << endl;
+	//Average contains time vector, 1000 nums: 8.65763e-05 seconds
+
+
+	//TASK 3
+
+	//double time_count = 0;
+	//for (size_t i = 0; i < 1000; i++) {
+	//	Set<int> a;
+	//	vector<int> vec = random(-10000, 10000, 1000, i);
+	//	size_t size = vec.size();
+	//	for (size_t j = 0; j < size; j++) {
+	//		a.insert(vec[j]);
+	//	}
+	//	auto start_time = std::chrono::high_resolution_clock::now();
+	//	a.insert(random(-1000, 1000, i));
+	//	auto end = std::chrono::high_resolution_clock::now();
+	//	time_count += std::chrono::duration<double>(end - start_time).count();
+	//}
+	//cout << "Average time, 1000 nums: "<<time_count / 1000 << " seconds"<< endl;
+	//Average time, 1000 nums: 6.8722e-06 seconds
 
 	return 0;
 }
--- include/aisd_set/aisd_set.h
+++ include/aisd_set/aisd_set.h
@@ -9,163 +9,172 @@ namespace set_realization {
 		T _val;
 		Node* _left;
 		Node* _right;
-		Node(T val, Node* left=nullptr, Node* right=nullptr) : _val(val), _left(left), _right(right) {}
+		Node(T val, Node* left = nullptr, Node* right = nullptr) : _val(val), _left(left), _right(right) {}
 	};
 
 	template <typename T>
 	class Set {
-		private:
-			Node<T>* _root;
-			size_t _size;
-		public:
-			Set() : _root(nullptr), _size(0) {}
-			Set(const Set& other) {
-				_root = create_copy_tree(other._root);
+	private:
+		Node<T>* _root;
+		size_t _size;
+	public:
+		Set() : _root(nullptr), _size(0) {}
+		Set(const Set& other) {
+			_root = create_copy_tree(other._root);
+		}
+		~Set() {
+			clear(_root);
+		}
+		Set& operator = (const Set& other) {
+			clear(_root);
+			_root = create_copy_tree(other._root);
+		}
+		void clear(Node<T>* head) {
+			if (!head) {
+				return;
 			}
-			~Set() {
-				clear(_root);
+			clear(head->_left);
+			clear(head->_right);
+			delete head;
+		}
+		Node<T>* create_copy_tree(Node<T>* root) {
+			if (!root) {
+				return nullptr;
 			}
-			Set& operator = (const Set& other){
-				clear(_root);
-				_root = create_copy_tree(other._root);
-			}
-			void clear(Node<T>* head) {
-				if (!head) {
-					return;
-				}
-				clear(head->_left);
-				clear(head->_right);
-				delete head;
-			}
-			Node<T>* create_copy_tree(Node<T>* root) {
-				if (!root) {
-					return nullptr;
-				}
 
-				Node<T>* new_node = new Node<T>(root->_val);
-				new_node->_left = create_copy_tree(root->_left);
-				new_node->_right = create_copy_tree(root->_right);
+			Node<T>* new_node = new Node<T>(root->_val);
+			new_node->_left = create_copy_tree(root->_left);
+			new_node->_right = create_copy_tree(root->_right);
 
-				return new_node;
-			}
-			Node<T>* get_root() const {
-				return _root;
-			}
-			bool insert(const int& val) {
-				Node<T>* new_node = new Node<T>(val);
-				if (!_root) {
-					_root = new_node;
-					return true;
-				}
-				Node<T>* cur = _root;
-				Node<T>* ptr = nullptr;
-				while (cur)
-				{
-					ptr = cur;
-					if (val < cur->_val) {
-						cur = cur->_left;
-					}
-					else if (val > cur->_val) {
-						cur = cur->_right;
-
-					}
-					else {
-						delete new_node;
-						return false;
-					}
-				}
-				if (val < ptr->_val) {
-					ptr->_left = new_node;
-				}
-				else {
-					ptr->_right = new_node;
-				}
+			return new_node;
+		}
+		Node<T>* get_root() const {
+			return _root;
+		}
+		bool insert(const int& val) {
+			Node<T>* new_node = new Node<T>(val);
+			if (!_root) {
+				_root = new_node;
 				return true;
 			}
-			void print() {
-				if (_root) {
-					recursion(_root);
+			Node<T>* cur = _root;
+			Node<T>* ptr = nullptr;
+			while (cur)
+			{
+				ptr = cur;
+				if (val < cur->_val) {
+					cur = cur->_left;
 				}
-				std::cout << std::endl;
-			}
-			bool contains(const int& val) {
-				if (!_root) {
-					return false;
-				}
-				Node<T>* cur = _root;
-				while (cur) {
-					if (cur->_val == val) {
-						return true;
-					}
-					else if(cur->_val<val) {
-						cur = cur->_right;
-					}
-					else if (cur->_val > val) {
-						cur = cur->_left;
-					}
-				}
-				return false;
-			}
-			void recursion(Node<T>* root) {
-				if (!root) {
-					return;
-				}
-				recursion(root->_left);
-				std::cout << root->_val << ' ';
-				recursion(root->_right);
-			}
-			bool erase(const int& val) {
-				return erase_with_root(_root, val);
-			}
-			bool erase_with_root(Node<T>*& root, const int& val) {
-				if (!root) {
-					return false;
-				}
+				else if (val > cur->_val) {
+					cur = cur->_right;
 
-				if (root->_val > val) {
-					return erase_with_root(root->_left, val);
-				}
-				else if (root->_val < val) {
-					return erase_with_root(root->_right, val);
-				}
-
-				if (!root->_left) {
-					Node<T>* temp = root->_right;
-					delete root;
-					root = temp;
-					return true;
-				}
-				else if(!root->_right) {
-					Node<T>* temp = root->_left;
-					delete root;
-					root = temp;
-					return true;
 				}
 				else {
-					Node<T>* succParent = root;
-					Node<T>* succ = root->_right;
-					while (succ->_left != NULL) {
-						succParent = succ;
-						succ = succ->_left;
-					}
-					if (succParent != root)
-						succParent->_left = succ->_right;
-					else
-						succParent->_right = succ->_right;
-
-					root->_val = succ->_val;
-
-					delete succ;
-					return true;
+					delete new_node;
+					return false;
 				}
 			}
+			if (val < ptr->_val) {
+				ptr->_left = new_node;
+			}
+			else {
+				ptr->_right = new_node;
+			}
+			return true;
+		}
+		void print() {
+			if (_root) {
+				recursion(_root);
+			}
+			std::cout << std::endl;
+		}
+		bool contains(const int& val) {
+			if (!_root) {
+				return false;
+			}
+			Node<T>* cur = _root;
+			while (cur) {
+				if (cur->_val == val) {
+					return true;
+				}
+				else if (cur->_val < val) {
+					cur = cur->_right;
+				}
+				else if (cur->_val > val) {
+					cur = cur->_left;
+				}
+			}
+			return false;
+		}
+		void recursion(Node<T>* root) {
+			if (!root) {
+				return;
+			}
+			recursion(root->_left);
+			std::cout << root->_val << ' ';
+			recursion(root->_right);
+		}
+		bool erase(const int& val) {
+			return erase_with_root(_root, val);
+		}
+		bool erase_with_root(Node<T>*& root, const int& val) {
+			if (!root) {
+				return false;
+			}
+
+			if (root->_val > val) {
+				return erase_with_root(root->_left, val);
+			}
+			else if (root->_val < val) {
+				return erase_with_root(root->_right, val);
+			}
+
+			if (!root->_left) {
+				Node<T>* temp = root->_right;
+				delete root;
+				root = temp;
+				return true;
+			}
+			else if (!root->_right) {
+				Node<T>* temp = root->_left;
+				delete root;
+				root = temp;
+				return true;
+			}
+			else {
+				Node<T>* succParent = root;
+				Node<T>* succ = root->_right;
+				while (succ->_left != NULL) {
+					succParent = succ;
+					succ = succ->_left;
+				}
+				if (succParent != root)
+					succParent->_left = succ->_right;
+				else
+					succParent->_right = succ->_right;
+
+				root->_val = succ->_val;
+
+				delete succ;
+				return true;
+			}
+		}
 	};
 
 	std::vector<int> random(int a, int b, size_t n, size_t i) {
+		std::vector<int> res;
 		std::mt19937 gen(i);
 		std::uniform_int_distribution<> distribution(a, b);
 		for (size_t j = 0; j < n; j++) {
-
+			size_t x = distribution(gen);
+			res.push_back(x);
 		}
+		return res;
+	}
+
+	int random(int a, int b, size_t i) {
+		std::mt19937 gen(i);
+		std::uniform_int_distribution<> distribution(a, b);
+		return distribution(gen);
 	}
 }
